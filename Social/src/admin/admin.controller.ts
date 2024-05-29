041c2b27d2ccdf816f7df9a09b163c1b717ad3b4
Ryder <mhieusz.23.dkc@gmail.com>
2024-05-29 16:22:05 +0700

    update

--- Social/src/admin/admin.controller.ts
+++ Social/src/admin/admin.controller.ts
@@ -1,35 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { ApiTags } from '@nestjs/swagger';
-
-
-
-
 
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {
-
-
-    @Post('view-comment-post')
-    getCommentFormPost(@Body() data: AllCommentPostDTO) {
-      try {
-        return this.commentService.getCommentFromPost(data);
-      } catch (error) {
-        throw new HttpException(
-          `Lỗi BE {deleteYourPost - getPostFromUser} ${error}`,
-          500,
-        );
-      }
+  constructor(private readonly adminService: AdminService) {}
+  @Get('view-list-user-waiting-to-accept')
+  viewListUSerWaitingToAccept() {
+    try {
+      // return this.adminService.getCommentFromPost(data);
+      return "Tối t4 có"
+    } catch (error) {
+      throw new HttpException(
+        `Lỗi BE {deleteYourPost - getPostFromUser} ${error}`,
+        500,
+      );
     }
+  }
 
 
-
-
-
-
-
-
+  
+  viewListReportPost() {
+    try {
+      // return this.adminService.getCommentFromPost(data);
+      return "Tối t4 có"
+    } catch (error) {
+      throw new HttpException(
+        `Lỗi BE {deleteYourPost - getPostFromUser} ${error}`,
+        500,
+      );
+    }
   }
 }
